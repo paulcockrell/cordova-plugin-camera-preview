da67508af1dad820cf6da3efaa9e28af7ab93121
Paul Cockrell <paulcockrell@gmail.com>
2016-07-03 22:12:50 +0100

    blah blah

--- src/android/com/mbppower/CameraActivity.java
+++ src/android/com/mbppower/CameraActivity.java
@@ -119,8 +119,8 @@ public class CameraActivity extends Fragment {
 
 		//text view
 	        metricsView = (TextView) view.findViewById(getResources().getIdentifier("metrics_view", "id", appResourcesPackage));
-		mainLayout.addView(metricsView, 10, 10);
 		metricsView.setText("Hi?");
+		mainLayout.addView(metricsView, 10, 10);
 
 	        final GestureDetector gestureDetector = new GestureDetector(getActivity().getApplicationContext(), new TapGestureDetector());
 

b4df529788061af733d7ca5228ec58df80bca1bc
Paul Cockrell <paulcockrell@gmail.com>
2016-07-12 21:11:13 +0100

    Test other way of drawing canvas from seperate threaderoo

--- src/android/com/mbppower/CameraActivity.java
+++ src/android/com/mbppower/CameraActivity.java
@@ -809,13 +809,13 @@ class Preview extends RelativeLayout implements SurfaceHolder.Callback {
         }
     }
 
-    //@Override
-    //protected void onDraw(Canvas canvas) {
-    //    Paint p = new Paint(Color.RED);
-    //    canvas.drawText("Preview", 10, 100, p);
-    //    Log.d(TAG, "XXX On draw called");
-    //    super.draw(canvas);
-    //}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint p = new Paint(Color.RED);
+        canvas.drawText("Preview", 10, 100, p);
+        Log.d(TAG, "XXX On draw called");
+        super.draw(canvas);
+    }
 
     public void surfaceDestroyed(SurfaceHolder holder) {
         // Surface will be destroyed when we return, so stop the preview.

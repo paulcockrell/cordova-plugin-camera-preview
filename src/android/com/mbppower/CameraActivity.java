121456477fef40ebcb05d7a814e4c50050ca1f87
Paul Cockrell <paulcockrell@gmail.com>
2016-07-12 09:47:06 +0100

    Constant

--- src/android/com/mbppower/CameraActivity.java
+++ src/android/com/mbppower/CameraActivity.java
@@ -402,7 +402,7 @@ public class CameraActivity extends Fragment {
                             Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
                             paint.setColor(Color.rgb(61,61,61));
                             paint.setTextSize((int) (14 * 2));
-                            paint.setShadowLayer(1f, 0f, 1f, Color.White);
+                            paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
                             Rect bounds = new Rect();
                             String text = "Hello";
                             paint.getTextBounds(text, 0, text.length(), bounds);
